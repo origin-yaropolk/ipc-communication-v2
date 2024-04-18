999f9cb35f74524350838adfb20ce8e98f224fdd
Egor Kushnarev <ekushnarev@tradingview.com>
2024-04-18 13:04:41 +0300

    reexport promisify

--- src/services-over-ipc/main/index.ts
+++ src/services-over-ipc/main/index.ts
@@ -1,2 +1,3 @@
 export { Service, ExposeService, ServiceLifeTime } from '../ipc-services/decorators';
 export { ServiceLocatorOverIpc } from '../ipc-services/initialization-main';
+export { Promisify } from '../ipc-communication/proxy/ipc-proxy';
